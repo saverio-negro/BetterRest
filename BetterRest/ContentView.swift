d195d83464e6e6fceec6ae2481d1528a8f260917
Saverio Negro <saverionegro@SaveriosMacBook.fritz.box>
2024-10-30 21:16:12 +0100

    Added Stepper for number of hours the user would like to sleep

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp: Date = Date.now
+    private var tomorrow: Date {
+        /// Return a `Date` instance set to one day (in seconds) fro now
+        return Date.now.addingTimeInterval(86400)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted(.number)) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please, enter a date.", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+                .labelsHidden()
         }
         .padding()
     }
