9216dfb673efc5b6d37f5e253d8fb905b172750d
Saverio Negro <saverionegro@10-20-52-162.0200.fed.it.iosda.org>
2024-10-30 12:11:49 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Saverio Negro on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Saverio Negro on 30/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
