f259b391d1eba6923255257d255168cf56b5a764
Saverio Negro <saverionegro@SaveriosMacBook.fritz.box>
2024-11-01 17:07:27 +0100

    Added DatePicker to track the time the user wants to wake up.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,19 +11,39 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var wakeUp: Date = Date.now
-    private var tomorrow: Date {
-        /// Return a `Date` instance set to one day (in seconds) fro now
-        return Date.now.addingTimeInterval(86400)
+    @State private var actualSleepDate: Date = Date.now
+    var defaultWakeUp: Date {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
     }
     
     var body: some View {
         VStack {
             Stepper("\(sleepAmount.formatted(.number)) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             
-            DatePicker("Please, enter a date.", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+            DatePicker("Please, enter a date.", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
+            HStack {
+                Text("You should sleep at")
+                Text(actualSleepDate, format: .dateTime.hour().minute())
+            }
         }
         .padding()
+        .onAppear {
+            wakeUp = defaultWakeUp
+            print(wakeUp)
+        }
+        .onChange(of: wakeUp) { oldValue, newValue in
+            
+            let components = Calendar.current.dateComponents(Set([.hour, .minute]), from: wakeUp)
+            let chosenHour = components.hour ?? 0
+            let chosenMinute = components.minute ?? 0
+            
+            print(chosenHour)
+            print(chosenMinute)
+        }
     }
 }
 
